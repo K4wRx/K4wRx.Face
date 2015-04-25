af6c60bd7619faa1c8df1b5256ae8e823f1e62dc
fumito_ito <weathercook@gmail.com>
2015-04-26 00:56:35 +0900

    initial commit

---
+++ K4wRx.Face/Extensions/FaceReaderExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Kinect;
+using K4wRx.Extensions;
+using Microsoft.Kinect.Face;
+
+namespace K4wRx.Extensions
+{
+    public static class FaceReaderExtension
+    {
+        public static IObservable<FaceFrameArrivedEventArgs> AsObservable(this KinectSensor sensor)
+        {
+            throw new Exception("this function is not enabled");
+        }
+    }
+}
---
+++ K4wRx.Face/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("K4wRx.Face")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("K4wRx.Face")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("13ca7c73-2bf5-4a9a-8013-b061cb7dfe09")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
